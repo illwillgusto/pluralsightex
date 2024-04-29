12bc175d7db4a811a219eee9fdb5c6c0428cc5ca
Will Walton <will@endless3dge.com>
2024-04-29 12:32:02 -0500

    Update index.js
    point for using comments, single line comments

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+// These are comments, Dylan keep in mind you want to use codes so other devs can understand the code 
+
 // Creating the name variables
 let firstName = "Will";
 let lastName = "Walton";
 
-// Logging name to the console 
+// Logging name to the console
 console.log(`Hello ${firstName} ${lastName}`);
