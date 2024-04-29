8e6828b680e447f4305eb19715ebab2993bf4e6c
Will Walton <will@endless3dge.com>
2024-04-29 13:54:58 -0500

    Update null-undefined.js
    undefined and the type of method examples

--- null-undefined.js
+++ null-undefined.js
@@ -1,3 +1,6 @@
 //  What happens when you don't give a variable a value
 let x;
 console.log(x);
+console.log(typeof(x)); // typeof will give the type of value the variable is set to
+x = "What's Up!";
+console.log(x);
