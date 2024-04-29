85926d05052d16db8d6d093702858e9f16d9dc8f
Will Walton <will@endless3dge.com>
2024-04-29 12:39:08 -0500

    Update index.js
    numTearsEmployement and dateBirth variables declared

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 // These are comments, Dylan keep in mind you want to use codes so other devs can understand the code
 
 /*
-This is a multiline comment syntax 
+This is a multiline comment syntax
 */
 
 // Creating the name variables
 let firstName = "Will";
 let lastName = "Walton";
+let numYearsEmployment = 1;
+let dateBirth = new Date("December 1, 1987");
 
 // Logging name to the console
 console.log(`Hello ${firstName} ${lastName}`);
