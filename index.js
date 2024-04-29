d9ce4ba728d279651464f447d3308c6292fc8415
Will Walton <will@endless3dge.com>
2024-04-29 12:33:10 -0500

    Update index.js
    multi line comments

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-// These are comments, Dylan keep in mind you want to use codes so other devs can understand the code 
+// These are comments, Dylan keep in mind you want to use codes so other devs can understand the code
+
+/*
+This is a multiline comment syntax 
+*/
 
 // Creating the name variables
 let firstName = "Will";
