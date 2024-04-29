fd03858276ca76f48acee0ae7061a9382067c547
Will Walton <will@endless3dge.com>
2024-04-29 14:10:40 -0500

    Update objects.js
    example of deleting props from an object

--- objects.js
+++ objects.js
@@ -26,4 +26,8 @@ console.log(obj3);
 // Accessing properties from an object
 console.log(`First Name: ${obj3.firstName}`);
 console.log(`Last Name: ${obj3.lastName}`);
-console.log(`Start Date: ${obj3.startDate}`);
+console.log(`Start Date: ${obj3["startDate"]}`);
+
+// Deleting props from an object
+delete obj3.vacationDays;
+console.log(obj3);
