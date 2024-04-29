de9de5f951035a4d48c08b03de65f005e9477354
Will Walton <will@endless3dge.com>
2024-04-29 13:56:19 -0500

    Update null-undefined.js
    null example

--- null-undefined.js
+++ null-undefined.js
@@ -4,3 +4,7 @@ console.log(x);
 console.log(typeof(x)); // typeof will give the type of value the variable is set to
 x = "What's Up!";
 console.log(x);
+
+// How do we set a variable to a empty value purposefully
+let manager = null;
+console.log(manager);5
