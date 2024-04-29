3fa36b7b2eb06e17fbdd78406cc6d0e7ccaa263f
Will Walton <will@endless3dge.com>
2024-04-29 13:48:24 -0500

    Create numbers.js
    Max and Min values and the limits of what can be contained numbers

---
+++ numbers.js
@@ -0,0 +1,18 @@
+// Define number values
+let num1 = 6;
+let num2 = -10000;
+let num3 = 1.2;
+
+// Defining big and small numbers
+let num4 = 1540000000000000000000000000;
+console.log(num4);
+
+// Creating number with e-notation
+let num6 = 1.2e+10;
+console.log(num6);
+
+// How big and small can we get with the number type
+console.log(`Minimum Value: ${Number.MIN_VALUE}`);
+console.log(`Minimum Safe Integer: ${Number.MIN_SAFE_INTEGER}`);
+console.log(`Maximum Value: ${Number.MAX_VALUE}`);
+console.log(`Maximum Safe Integer: ${Number.MAX_SAFE_INTEGER}`);
