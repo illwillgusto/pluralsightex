5c89c86d25e36c2942f79ddee7062c564cc3c468
Will Walton <will@endless3dge.com>
2024-04-29 13:29:36 -0500

    Update booleans.js
    not operator examples with booleans

--- booleans.js
+++ booleans.js
@@ -4,3 +4,8 @@ console.log(trueValue);
 let falseValue = false;
 console.log(falseValue);
 
+// Using the NOT operator with Booleans
+let notTrue = !true;
+console.log(notTrue);
+let notFalse = !false;
+console.log(notFalse);
