d0efb54016785c360acec2052f4e2010d24b4fbb
Will Walton <will@endless3dge.com>
2024-04-29 14:03:32 -0500

    Create objects.js
    create and accessing properties on an object and the 2 methods of doing so

---
+++ objects.js
@@ -0,0 +1,13 @@
+// Create an Object with an object literal (preferred)
+let obj1 = {};
+
+// Create an Object with a constructor
+let obj2 = new Object();
+
+// Populate properties on an object
+obj1.firstName = "Dylan";
+obj1.lastName = "Ray";
+obj1.isActive = true;
+obj1.startDate = new Date("April 29, 2024");
+obj1.vacationDays = 26;
+console.log(obj1);
