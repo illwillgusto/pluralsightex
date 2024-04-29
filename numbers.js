2c46de80990fb584cee4f4fdf370dc6ac35fc31d
Will Walton <will@endless3dge.com>
2024-04-29 13:51:06 -0500

    Update numbers.js
    BigInt examples

--- numbers.js
+++ numbers.js
@@ -16,3 +16,7 @@ console.log(`Minimum Value: ${Number.MIN_VALUE}`);
 console.log(`Minimum Safe Integer: ${Number.MIN_SAFE_INTEGER}`);
 console.log(`Maximum Value: ${Number.MAX_VALUE}`);
 console.log(`Maximum Safe Integer: ${Number.MAX_SAFE_INTEGER}`);
+
+// Creating BigInt's
+let bigInt1 = 1n;
+let bigInt = 1_560_000_000_000_000_000_000n;
