150e28f412bb6bcc5c3cd3a9ba47dfe81d5589ca
Will Walton <will@endless3dge.com>
2024-04-30 15:55:47 -0500

    Create conversion.js
    typeof function example and how it works within JS

---
+++ conversion.js
@@ -0,0 +1,7 @@
+// Using the typeof function
+let strValue = "Hi";
+console.log(typeof(strValue)); // string
+let numValue = 1;
+console.log(typeof(numValue)); // number
+let boolValue = true;
+console.log(typeof(boolValue)); // boolean 
