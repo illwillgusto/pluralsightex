08f8a06c55fbc3f4fd9a8b28a6af421bb611d55c
Will Walton <will@endless3dge.com>
2024-04-30 15:59:59 -0500

    Update conversion.js
    example of taking a non-string and combining it with a string
    using temporal literals this will then become a string

--- conversion.js
+++ conversion.js
@@ -4,4 +4,11 @@ console.log(typeof(strValue)); // string
 let numValue = 1;
 console.log(typeof(numValue)); // number
 let boolValue = true;
-console.log(typeof(boolValue)); // boolean 
+console.log(typeof(boolValue)); // boolean
+
+// Joining a non-string value with a string
+let age = 21;
+let firstName = "Bruce";
+let description = `${firstName} is ${age} years old.`; // using temporal literals to combine the 2 to make a string
+console.log(description);
+console.log(typeof(description));
