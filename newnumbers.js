aed2b0c038fc618d38a854bdf4e01194fcef9ca6
Will Walton <will@endless3dge.com>
2024-04-30 16:39:52 -0500

    Create newnumbers.js
    rounding of numbers and the different math methods that can be used to accomplish your specifics for rounding

---
+++ newnumbers.js
@@ -0,0 +1,9 @@
+// Rounding number to an  integer
+let num1 = 5.618345;
+console.log(`Round: ${Math.round(num1)}`); // normal rounding up
+console.log(`Ceiling: ${Math.ceil(num1)}`); // first number past the value
+console.log(`Floor: ${Math.floor(num1)}`); // first number before the value
+
+
+// Fixed
+
