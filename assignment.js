9b4705963a71b6858934ab5c7ec7aaed274742f8
Will Walton <will@endless3dge.com>
2024-05-01 11:26:53 -0500

    Create assignment.js
    assignment operators and with mathematic assignments

---
+++ assignment.js
@@ -0,0 +1,28 @@
+// Assignment
+let x = "this is a string";
+let y = 42;
+let z = false;
+let date = new Date();
+
+// Mathematical Assignment
+let val1 = 3;
+val1 += 6; // Same thing as val1 = val1 + 6;
+console.log(`val1 : ${val1}`);
+
+let val2 = 2;
+val2 -= 1;
+console.log(`val2: ${val2}`);
+
+let val3 = 6;
+val3 *= 2;
+console.log(`val3: ${val3}`);
+
+
+// Assignment with increment and decrement operators
+let val7 = 10;
+let val8 = val7++;
+console.log(`val7: ${val7} val8: ${val8}`);
+
+let val9 = 10;
+let val10 = --val9;
+console.log(`val9: ${val9} val10: ${val10}`);
