23c2bb4b3d04ba58dd62ad47fcfd817a12d2c587
Will Walton <will@endless3dge.com>
2024-05-01 11:06:22 -0500

    Create math.js
    math operators introduced

---
+++ math.js
@@ -0,0 +1,40 @@
+// Addition
+let val1 = 3 + 2;
+console.log(val1);
+let val2 = 2 + -1;
+console.log(val2);
+
+// Subtraction
+let val3 = 4 - 1;
+console.log(val3);
+let val4 = 1 - -1;
+console.log(val4);
+
+// Division
+let val5 = 21 / 7;
+console.log(val5);
+let val6 = 35 / 7;
+console.log(val6);
+
+// Multiplication
+let val7 = 3 * 2;
+
+let val8 = 3 * 3;
+
+// Remainder
+let cal9 = 9 % 2;
+console.log(val8);
+let val10 = -10 % 2;
+
+// Exponentiation
+let val11 = 3 ** 2;
+console.log(val11);
+
+// Increment and decrement
+let val13 = 1;
+++val13; // increment syntax, to increase the value by 1
+console.log(`val13: ${val13}`); // 2
+
+let val14 = 1;
+--val14; // decrement syntax, to decrease the value by 1
+console.log(`val14: ${val14}`); // 0
