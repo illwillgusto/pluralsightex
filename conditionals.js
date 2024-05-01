d03d880deec909752dc835952368f46aa17b8357
Will Walton <will@endless3dge.com>
2024-05-01 10:56:18 -0500

    Create conditionals.js
    conditional if else statements
    provided an example of a if statement with conditions of else if to determine the employees badge color

---
+++ conditionals.js
@@ -0,0 +1,14 @@
+// Using conditionals
+let badgeColor;
+
+if (numYearsService < 5) {
+  badgeColor = "blue";
+} else if (numYearsService < 10) {
+  badgeColor = "yellow";
+} else if (numYearsService < 15) {
+  badgeColor = "black";
+} else if (numYearsService < 20) {
+  badgeColor = "silver";
+} else {
+  badgeColor = "gold";
+}
