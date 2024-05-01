3cff29f3af052d0a330470caa7a1fbec2b62b409
Will Walton <will@endless3dge.com>
2024-05-01 12:16:54 -0500

    Update conditionals.js
    conditional statement examples given in more depth and the benefits of using the conditions to check for data validation from an object's properties

--- conditionals.js
+++ conditionals.js
@@ -1,14 +1,54 @@
 // Using conditionals
+
+
+// Employee
+let employee = {
+  firstName: "Will",
+  lastName: "Walton",
+  startDate: new Date(2024, 5, 13),
+  numYearsService: 1,
+  isActive: true,
+  department: "Engineering",
+  title: "Solutions Engineer"
+};
+
+// Badge Color with employee object
 let badgeColor;
 
-if (numYearsService < 5) {
-  badgeColor = "blue";
-} else if (numYearsService < 10) {
-  badgeColor = "yellow";
-} else if (numYearsService < 15) {
-  badgeColor = "black";
-} else if (numYearsService < 20) {
-  badgeColor = "silver";
+if (employee.numYearsService < 5) {
+  employee.badgeColor = "blue";
+} else if (employee.numYearsService < 10) {
+  employee.badgeColor = "yellow";
+} else if (employee.numYearsService < 15) {
+  employee.badgeColor = "black";
+} else if (employee.numYearsService < 20) {
+  employee.badgeColor = "silver";
 } else {
-  badgeColor = "gold";
+  employee.badgeColor = "gold";
 }
+console.log(`Years: ${employee.numYearsService} Badge Color: ${employee.badgeColor}`);
+
+
+// Evaluating multiple conditions for truthyness in all conditions
+if(employee.numYearsService < 5 && employee.department === "Engineering") {
+  console.log(`Employee meets criteria`);
+}
+
+// Evaluating multiple conditions for truthyness in one or more conditions
+if(employee.numYearsService > 1 || employee.title.startsWith("Solutions")) {
+  console.log(`Employee meets criteria`);
+}
+
+// Evaluating for a condition to NOT be true
+if(employee.numYearsService > 20 && !employee.isActive) {
+  console.log(`Employee is retiring soon with full retirement benefits`);
+}
+
+// Using the ternary operator (? :)
+if(employee.numYearsService > 10) {
+  employee.annualBonus = 2500;
+} else {
+  employee.annualBonus = 1500;
+}
+
+employee.annualBonus = (employee.numYearsService > 10) ? 2500 : 1500; // ? = then : = else
