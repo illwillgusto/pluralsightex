327e661462bd569b60b0e0b5d38f02f5e0f97133
Will Walton <will@endless3dge.com>
2024-05-08 13:27:52 -0500

    loop statements
    purpose of loops added to the loop.js
    
    installed package.json file added the module as type

---
+++ loop.js
@@ -0,0 +1,5 @@
+/*
+loop executes a block of statements until a specific condition is met.
+Once such condition could be reaching the end of a collection, but it also could
+be any other conditional you could think of.
+key words = for & while */
