1c225782f8fbf2d8e8ffad8492d0d6e9ff23507b
Will Walton <will@endless3dge.com>
2024-05-08 13:59:26 -0500

    Update while-loops.js
    while loop to output the names of the employees and display them in the console

--- while-loops.js
+++ while-loops.js
@@ -1,2 +1,9 @@
 // Array of Complex Objects
 import employees from "./data.json" assert { type: 'json' }
+
+// Basic while loop
+let i = 0;
+while (employees[i]) {
+  console.log(`Name: ${employees[i].firstName} ${employees[i].lastName}`);
+  i++;
+}
