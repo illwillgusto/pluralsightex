95df3c1025f43be898b794d878144dfcd487645a
Will Walton <will@endless3dge.com>
2024-05-09 17:01:49 -0500

    Update functions-notes.js
    comment explaining HOFs

--- functions-notes.js
+++ functions-notes.js
@@ -14,5 +14,19 @@ function scope means the variables inside that function cannot be used outside o
 Variables declared outside a function are in a global scope
 
 functions in JS only return a single value
-functions don't have to have a return value 
+functions don't have to have a return value
+
+
+
+
+
+
+
+
+
+
+
+
+Higher-order Functions
+takes one or more parameters and returns another function as its return value 
  */
