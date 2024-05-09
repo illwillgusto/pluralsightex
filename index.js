0c30d29361b8cd08d0e2d14c1402cfb4d308c2b8
Will Walton <will@endless3dge.com>
2024-05-09 08:49:20 -0500

    new functions declared
    function created for listEmployees with declared variables
    
    function declared for adding employees with declared variables within the function scope

--- index.js
+++ index.js
@@ -1,14 +1,117 @@
-// These are comments, Dylan keep in mind you want to use codes so other devs can understand the code
+// Import Sample Data
+import employees from './data.json' assert { type: 'json' }
 
-/*
-This is a multiline comment syntax
-*/
+import createPrompt from 'prompt-sync';
+let prompt = createPrompt();
 
-// Creating the name variables
-let firstName = "Will";
-let lastName = "Walton";
-let numYearsEmployment = 1;
-let dateBirth = new Date("December 1, 1987");
 
-// Logging name to the console
-console.log(`Hello ${firstName} ${lastName}`);
+
+// Application commands
+
+function listEmployees() {
+  console.log(`Employee List ----------------------------`);
+    console.log('');
+
+    for (let emp of employees) {
+      for (let property in emp) {
+        console.log(`${property}: ${emp[property]}`);
+      }
+      console.log('');
+      prompt('Press enter to continue...');
+    }
+    console.log(`Employee list completed`);
+}
+
+function addEmployee() {
+  console.log(`Add Employee -----------------------------`);
+    console.log('');
+    let employee = {};
+
+    let firstName = prompt("First Name: ");
+    if (!firstName) {
+      console.error(`Invalid first name`);
+      process.exit(1);
+    }
+    employee.firstName = firstName;
+
+    let lastName = prompt("Last Name: ");
+    if (!lastName) {
+      console.error(`Invalid last name`);
+      process.exit(1);
+    }
+    employee.lastName = lastName;
+
+    let startDateYear = prompt("Employee Start Year (1990-2023): ");
+    startDateYear = Number(startDateYear);
+    // Check if it is a valid integer
+    if (!Number.isInteger(startDateYear)) {
+      console.error(`Enter a valid start date year`);
+      process.exit(1);
+    }
+    // Check if the number is in the range
+    if (startDateYear < 1990 || startDateYear > 2023) {
+      console.error(`Enter a start date year within the correct range`);
+      process.exit(1);
+    }
+
+    let startDateMonth = prompt("Employee Start Date Month (1-12): ");
+    startDateMonth = Number(startDateMonth);
+    // Check if it is a valid integer
+    if (!Number.isInteger(startDateMonth)) {
+      console.error(`Enter a valid start date month`);
+      process.exit(1);
+    }
+    // Check if the number is in the range
+    if (startDateMonth < 1 || startDateMonth > 12) {
+      console.error(`Enter a start date month within the correct range`);
+      process.exit(1);
+    }
+
+    let startDateDay = prompt("Employee Start Date Day (1-31): ");
+    startDateDay = Number(startDateDay);
+    // Check if it is a valid integer
+    if (!Number.isInteger(startDateDay)) {
+      console.error(`Enter a valid start date day`);
+      process.exit(1);
+    }
+    // Check if the number is in the range
+    if (startDateDay < 1 || startDateDay > 31) {
+      console.error(`Enter a start date day within the correct range`);
+      process.exit(1);
+    }
+
+    // Date elements are correct, let's create the date
+    employee.startDate = new Date(startDateYear, startDateMonth - 1, startDateDay);
+
+    let isActive = prompt("Is employee active (yes or no): ");
+    // Check if incorrect value was entered
+    if (isActive !== "yes" && isActive !== "no") {
+      console.error(`Enter yes or no for employee active status`);
+      process.exit(1);
+    }
+    employee.isActive = (isActive === "yes");
+
+    // Output Employee JSON
+    const json = JSON.stringify(employee, null, 2);
+    console.log(`Employee: ${json}`);
+}
+// Application execution
+
+// Get the command the user wants to exexcute
+const command = process.argv[2].toLowerCase();
+
+switch (command) {
+
+  case 'list':
+    listEmployees();
+    break;
+
+  case 'add':
+    addEmployee();
+    break;
+
+  default:
+    console.log('Unsupported command. Exiting...');
+    process.exit(1);
+
+}
