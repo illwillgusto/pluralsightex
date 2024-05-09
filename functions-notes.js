8142e10ac852d18506878d575dbc0bcd4573b8bd
Will Walton <will@endless3dge.com>
2024-05-09 08:39:52 -0500

    Create functions-notes.js
    function key notes to remember

---
+++ functions-notes.js
@@ -0,0 +1,18 @@
+/*
+Function declaration
+function isInputValidDepartment (input) {
+  console.log("Validating Department");
+}
+
+Executing a function
+isInputValidDepartment("Marketing");
+
+
+Scope -
+function scope means the variables inside that function cannot be used outside of that function
+
+Variables declared outside a function are in a global scope
+
+functions in JS only return a single value
+functions don't have to have a return value 
+ */
