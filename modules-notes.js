802b25db18bb0a1de94d0d41b330fa7d22b7f729
Will Walton <will@endless3dge.com>
2024-05-11 14:13:50 -0500

    Create modules-notes.js
    module examples
    
    CommonJS module example
    
    EMS module example which is more commonly used now

---
+++ modules-notes.js
@@ -0,0 +1,41 @@
+/*
+CommonJS Modules
+-Standard created for Node.js
+-Load modules using the require method
+-Uses an exports variable to determine exports
+
+
+ESM Modules (ECMAScript Modules)
+-Load modules using the import keyword
+-Uses the export keyword to determine modules
+
+CommonJS example:
+const fs = require("node:fs/promises");
+const localFile = require("./myFile");
+const sampleJSON = require("./sample.json");
+
+const loadData = async () => {
+  const data = await fs.readFile("./data.json");
+  return JSON.parse(data);
+};
+
+module.exports.jsonData = sampleJSON;
+module.exports.loadData = loadData;
+
+
+ESM example:
+import { readFile } from "node:fs/promises";
+import * as local from "./myFile.js";
+import sampleJSON from "./sample.json" assert { type: "json" };
+
+const loadData = async () => {
+  const data = await fs.readFile("./data.json");
+  return JSON.parse(data);
+};
+
+export {
+  jsonData: sampleJSON,
+  loadData
+};
+
+  */
