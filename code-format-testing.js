d1b4948a18ad32f20b2397cad14a7a179d55cef7
Will Walton <will@endless3dge.com>
2024-05-12 19:31:02 -0500

    Create code-format-testing.js
    unit testing notes, explaining the purpose of using unit testing

---
+++ code-format-testing.js
@@ -0,0 +1,10 @@
+/*
+Unit Testing
+where individual components ("units") of an application are tested to make sure
+they behave as the author intended.
+These tests are usually written for a specific unit testing framework.
+It is common for these tests to get executed every time code is committed to a
+project to ensure the new code does not break application functionality.
+
+-unit testing is just one type of testing that complex software projects may leverage
+*/
